afb3ecaca79e79335543dd281768e58dc7acc09f
MauricioHz <mauriciohz2002@gmail.com>
2018-07-06 11:38:00 -0400

    ok

--- CONSULTAS_GESTION_NC.sql
+++ CONSULTAS_GESTION_NC.sql
@@ -25,3 +25,30 @@ SELECT * FROM gestion_eficacia WHERE numero_registro = 'NC225-2018'
 
 -- NOTA
 -- Falta revisar el tercer check del listado.
+
+-- cambiar consulta
+
+SELECT
+	A.idnc AS id_noconformidad,
+	A.num_reg AS numero_registro,
+	DATE_FORMAT( A.fec_det, '%d-%m-%Y' ) AS fecha_deteccion,
+	DATE_FORMAT( A.fec_imp, '%d-%m-%Y' ) AS fecha_implementacion,
+	A.tip_hall AS tipo_hallazgo,
+	A.pcs_afe AS proceso_afectado,	
+	A.url_imf_adj AS archivo_adjunto_1,
+	A.url_imf_adj2 AS archivo_adjunto_2,
+	A.estado AS estado,
+	A.usuario AS usuario,
+	(CASE WHEN des_ai_imp = '' OR fec_imp = '0000-00-00' THEN '0' ELSE '1' END ) AS accion_inmediata,
+	IF( B.numero_registro IS NULL, '0', '1' ) AS analisis_causa,
+	IF( C.numero_registro IS NULL, '0', '1' ) AS verifica
+FROM gestion_noconformidad AS A
+LEFT JOIN gestion_analisis_causa AS B 
+ON A.num_reg = B.numero_registro
+LEFT JOIN gestion_verifica AS C 
+ON A.num_reg = C.numero_registro
+WHERE A.estado IN( 1, 2, 4 )
+GROUP BY A.idnc, A.num_reg, fecha_deteccion, fecha_implementacion, A.tip_hall,
+A.pcs_afe, A.url_imf_adj, A.url_imf_adj2, A.estado, A.usuario, accion_inmediata,
+analisis_causa, verifica
+ORDER BY num_reg DESC
